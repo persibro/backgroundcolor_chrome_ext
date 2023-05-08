5e7eb50f815bb52928f3b75b7b14a1c91ab349df
P V <persimmon.brother@gmail.com>
2023-05-08 15:58:04 -0700

    split off CSS, and added instructions to the page

--- backgroundColor.js
+++ backgroundColor.js
@@ -1,14 +1,26 @@
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  
+  return color;
+}
+
+function fadeOutInfoBox() {
+  const infoBox = document.getElementById('infoBox');
+  infoBox.classList.add('fade-out');
+  setTimeout(() => {
+    infoBox.style.display = 'none';
+  }, 1000);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-document.body.addEventListener('click', () => {
+  document.body.addEventListener('click', () => {
     document.body.style.backgroundColor = getRandomColor();
-});
+  });
+
+  setTimeout(() => {
+    fadeOutInfoBox();
+  }, 10000);
 });
